c0d4f43902064108f0ab4fa935f54cf4598a243b
ryaneric100 <42066789+ryaneric100@users.noreply.github.com>
2024-02-08 12:11:34 +0100

    deploy test

---
+++ src/app.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 22:17:56 2023
+
+@author: DCHELD
+"""
+
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash(__name__)
+server = app.server
+
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+if __name__ == '__main__':
+    app.run(debug=False)
+    server = app.server # required for some deployment environment like Heroku
+    #app.run_server(port=8080,debug=False)
+	
